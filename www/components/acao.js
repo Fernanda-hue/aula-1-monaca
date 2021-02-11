d95b1e9deb291eec445a1acbc31298c1683e48d9
Fernanda-hue <jonathanpaulinosantana1999@gmail.com>
2021-02-11 09:25:15 +0900

    aplicativo DEUS tenha piedade de mim

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,10 +1,10 @@
 // This is a JavaScript file
 
 $(document).on("click","#somar",function(){ 
-    var valor1 = $("#valor1").val ();
-    var valor2 = $("#valor2").val ();
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
 
-    var resultado = parseFloat(Valor1) + parseFloat(Valor2);
+    var resultado = parseFloat(valor1) + parseFloat(valor2);
 
     $("#resultado").val(resultado);
 });
